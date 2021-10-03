bdf6c35eda79dfe80f56d5a6b45bacad38dcfeb7
Dan Storey <danstorey901@gmail.com>
2021-10-03 16:51:58 -0400

    Add project files.

---
+++ CSHARP-SQLTUT/Program.cs
@@ -0,0 +1,26 @@
+﻿
+using Microsoft.Data.SqlClient;
+using System;
+
+namespace CSHARP_SQLTUT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connStr = "server=localhost\\sqlexpress;database=EdDb;trusted_connection=true;";
+            var sqlConn = new SqlConnection(connStr);
+            sqlConn.Open();
+            if (sqlConn.State != System.Data.ConnectionState.Open)
+            {
+                Console.WriteLine("Connection did not open");
+                return;
+            }
+            Console.WriteLine("Connection opened.");
+            // continue sql here
+
+            sqlConn.Close();
+
+        }
+    }
+}
